58bfa1589a9a4aad1ef8d490a8f632cfd9205067
SuperBaZhua <729123353@qq.com>
2017-08-01 22:48:31 +0800

    提交代码

---
+++ src/components/Pos.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    {{msg}}
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+    export default {
+      name: 'pos',
+      data () {
+        return{
+          msg: 123
+        }
+      }
+    }
+</script>
+
+<style>
+
+</style>
